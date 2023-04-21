444510be5deffdc4ad93020863d56eedb71ec45d
yangxingyuan <yangxingyuan@bytedance.com>
2023-04-21 11:31:51 +0800

    feat: init

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,8 @@
 import { Outlet } from '@modern-js/runtime/router';
+import hl from 'highlight.js';
 
+// eslint-disable-next-line no-console
+console.log(hl);
 export default function Layout() {
   return (
     <div>
